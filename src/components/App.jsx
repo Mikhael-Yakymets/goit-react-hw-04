5ced8ad782d1529a95f5f29ad2a5c45a8164d112
Mykhailo Yakymets <vskitano@gmail.com>
2025-05-16 01:00:50 +0300

    add search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { PulseLoader } from 'react-spinners';
 import axios from 'axios';
 import css from './App.module.css';
@@ -6,7 +6,9 @@ import SearchBar from './SearchBar/SearchBar';
 import ImageGallary from './ImageGallary/ImageGallary';
 
 const API_KEY = 'aK8HPseKZIXZIZbPdDbmdVjgvbFJ-gF6psHpwi1mgjo';
-const BASE_URL = 'https://api.unsplash.com/photos/';
+// const BASE_URL = 'https://api.unsplash.com/photos/';
+const BASE_URL = 'https://api.unsplash.com/';
+const PAGE_SIZE = 10;
 
 axios.defaults.baseURL = BASE_URL;
 
@@ -14,29 +16,55 @@ const App = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function fetchArticles() {
-      try {
-        // 1. Встановлюємо індикатор в true перед запитом
-        setLoading(true);
-        const response = await axios.get(`?client_id=${API_KEY}`);
-        setArticles(response.data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        // 2. Встановлюємо індикатор в false після запиту
-        setLoading(false);
-      }
-    }
+  // useEffect(() => {
+  //   async function fetchArticles() {
+  //     try {
+  //       // 1. Встановлюємо індикатор в true перед запитом
+  //       setLoading(true);
+  //       const response = await axios.get(`?client_id=${API_KEY}`);
+  //       setArticles(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       // 2. Встановлюємо індикатор в false після запиту
+  //       setLoading(false);
+  //     }
+  //   }
 
-    fetchArticles();
-  }, []);
+  //   fetchArticles();
+  // }, []);
+
+  const handleSearch = async (newTopic, currentPage = 1) => {
+    const params = {
+      client_id: API_KEY,
+      query: newTopic,
+      page: currentPage,
+      per_page: PAGE_SIZE,
+    };
+
+    try {
+      // 1. Встановлюємо індикатор в true перед запитом
+      setLoading(true);
+      const response = await axios.get(BASE_URL + 'search/photos/', {
+        params: {
+          ...params,
+        },
+      });
+      setArticles(response.data.results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // 2. Встановлюємо індикатор в false після запиту
+      setLoading(false);
+    }
+  };
+
   return (
     <div className={css.container}>
-      <SearchBar />
-      <h1>Latest articles</h1>
+      <SearchBar onSearch={handleSearch} />
+
       {loading && <PulseLoader color="#ff3b70" margin={2} size={22} />}
-      {<ImageGallary items={articles} />}
+      {articles.length > 0 && <ImageGallary items={articles} />}
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,27 @@
 import css from './SearchBar.module.css';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const form = evt.target;
+    const topic = form.elements.topic.value;
+    // Error
+    if (form.elements.topic.value.trim() === '') {
+      alert('Please enter search term!');
+      return;
+    }
+
+    onSearch(topic);
+    form.reset();
+  };
+
   return (
     <header>
-      <form className={css.form}>
+      <form className={css.form} onSubmit={handleSubmit}>
         <input
           className={css.formSearch}
           type="text"
+          name="topic"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
